a50692d9ad4d8685cda9d62b80a660f711e46238
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-02-12 23:41:04 +0900

    Update Linkareer.py

--- Linkareer.py
+++ Linkareer.py
@@ -4,6 +4,31 @@ import sqlite3
 
 
 
+class stack():
+    
+    def __init__(self):
+        
+        self.stack = []
+        
+    def __str__(self):
+        
+        return str(self.stack)
+    
+    def push(self, value):
+        
+        self.stack.append(value)
+        
+    def pop(self):
+        
+        if self.isEmpty():  return IndexError("Stack Empty")
+        else:   self.stack.pop()
+
+    def isEmpty(self):
+        
+        return len(self.stack) == 0
+
+
+
 def head(page_num):
     
     url = f"https://api.linkareer.com/graphql?operationName=RecruitList&variables=%7B%22filterBy%22%3A%7B%22status%22%3A%22OPEN%22%2C%22activityTypeID%22%3A%225%22%2C%22categoryIDs%22%3A%5B%22111%22%5D%2C%22regionIDs%22%3A%5B%222%22%2C%229%22%2C%225%22%5D%2C%22jobTypes%22%3A%5B%22INTERN%22%5D%7D%2C%22activityOrder%22%3A%7B%22field%22%3A%22RECENT%22%2C%22direction%22%3A%22DESC%22%7D%2C%22page%22%3A{page_num}%2C%22pageSize%22%3A20%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%221de6571120f5792968cfc5b56c6ee631a59d588ab75051708d13907844dd87b2%22%7D%7D"
@@ -13,7 +38,7 @@ def head(page_num):
     
     return url, header
 
-def re(page_num):
+def re(page_num, id_num):
     
     url, header = head(page_num)
     
@@ -30,15 +55,23 @@ def re(page_num):
             
             id = node.get('id', {})
             
-            print(id)
+            id_num.push(id)
             
     else:   return 1
 
+def each_id():
 
-page_num = 0
-while True:
-    
-    page_num+=1
-    check = re(page_num)
-    
-    if check:   break
+    id_num = stack()
+    page_num = 0
+    while True:
+        
+        page_num+=1
+        check = re(page_num, id_num)
+        
+        if check:  break
+        
+    return id_num
+
+
+id_num = each_id()
+print(id_num)
