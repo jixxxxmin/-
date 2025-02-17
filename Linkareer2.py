2f8fff2d698612800f5d78c6e1f554860802dc63
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-02-17 23:55:28 +0900

    Update Linkareer2.py

--- Linkareer2.py
+++ Linkareer2.py
@@ -2,7 +2,19 @@ import requests
 import time
 
 
-ss = requests.Session()
+
+def session_sets():
+    
+    s = requests.Session()
+    set_url = "https://naver.com"
+    get_url = "https://naver.com"
+
+    s.get(set_url, params={"userName": "John99"})
+    s.get(set_url, params={"location": "NewYork"})
+
+    response = s.get(get_url)
+    print(response.text)
+
 
 ss_s = time.time()
 for _ in range(20):
@@ -17,5 +29,6 @@ for _ in range(20):
     
     response = requests.get("https://naver.com")
 e = time.time()
+
 
 print(e - s)
