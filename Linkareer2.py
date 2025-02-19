87dbe878bef9a0e52ae4f738835e980fe6e77fdb
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-02-19 23:42:41 +0900

    Update Linkareer2.py

--- Linkareer2.py
+++ Linkareer2.py
@@ -4,10 +4,13 @@ import time
 
 
 
-url = f"https://api.linkareer.com/graphql?operationName=RecruitList&variables=%7B%22filterBy%22%3A%7B%22status%22%3A%22OPEN%22%2C%22activityTypeID%22%3A%225%22%2C%22categoryIDs%22%3A%5B%22111%22%5D%2C%22regionIDs%22%3A%5B%222%22%2C%229%22%2C%225%22%5D%2C%22jobTypes%22%3A%5B%22INTERN%22%5D%7D%2C%22activityOrder%22%3A%7B%22field%22%3A%22RECENT%22%2C%22direction%22%3A%22DESC%22%7D%2C%22page%22%3A1%2C%22pageSize%22%3A20%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%221de6571120f5792968cfc5b56c6ee631a59d588ab75051708d13907844dd87b2%22%7D%7D"
+def info(page_num):
     
+    url = f"https://api.linkareer.com/graphql?operationName=RecruitList&variables=%7B%22filterBy%22%3A%7B%22status%22%3A%22OPEN%22%2C%22activityTypeID%22%3A%225%22%2C%22categoryIDs%22%3A%5B%22111%22%5D%2C%22regionIDs%22%3A%5B%222%22%2C%229%22%2C%225%22%5D%2C%22jobTypes%22%3A%5B%22INTERN%22%5D%7D%2C%22activityOrder%22%3A%7B%22field%22%3A%22RECENT%22%2C%22direction%22%3A%22DESC%22%7D%2C%22page%22%3A{page_num}%2C%22pageSize%22%3A20%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%221de6571120f5792968cfc5b56c6ee631a59d588ab75051708d13907844dd87b2%22%7D%7D"
+    
+    return url
 
-def session_sets():
+def session_sets(url):
     
     s = requests.Session()
     set_url = url
@@ -32,6 +35,15 @@ def session_sets():
             
     else:   return 1
 
+    return 0
 
 
-session_sets()
+page_num = 0
+while True:
+    
+    page_num += 1
+    url = info(page_num)
+    chk = session_sets(url)
+
+    if chk == 1:    break
+        
